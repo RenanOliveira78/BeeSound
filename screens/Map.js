f537142b932caaa42e619cb918b0aa5d4bb9f496
RenanRibeiro78 <renanribeiro78@alu.ufc.br>
2024-09-23 16:40:07 -0300

    one more marker

--- screens/Map.js
+++ screens/Map.js
@@ -21,7 +21,9 @@ export default function Map() {
     const locations = [
       { id: 1, latitude: -3.7427479332184883, longitude: -38.572757891503485 }, 
       { id: 2, latitude: -3.7432055848289423, longitude: -38.57285170215977 }, 
-      { id: 3, latitude: -3.7442457012359616, longitude: -38.571517506159324 }, 
+      { id: 3, latitude: -3.7442457012359616, longitude: -38.571517506159324 },
+      { id: 4, latitude: -3.74824570, longitude: -38.573517506 }, 
+
     ];
 
     const handleMarkerPress = (id) => {
